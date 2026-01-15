b13d90e52eb36c7cbc31524e8534221e0d87a147
zach.lewis_clarity <zlewis@clarityinnovates.com>
2026-01-15 09:56:51 -0600

    a

---
+++ loadmodel.sh
@@ -0,0 +1,5 @@
+docker exec ollama ollama pull mistral
+docker exec ollama ollama run mistral
+docker exec ollama ollama run mistral "python app"
+docker exec ollama ollama run mistral "create hello world app"
+docker exec ollama ollama run mistral "create java spring dto cdocker exec ollama ollama pull mistrallass"
